399102d916c993c89f615d192ffb2402c48bbaca
HaileabT <haileabtesfaye11@gmail.com>
2025-07-16 16:19:57 -0400

    added crud operation to the book model

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string',
+            'author' => 'required|string',
+            'year' => 'required|integer',
+            'pages' => 'required|integer',
+            'description' => 'required|string',
+        ]);
+
+        return Book::create($validated);
+    }
+
+   public function show($id)
+{
+    $book = Book::find($id);
+
+
+    return $book;
+}
+
+    public function update(Request $request, Book $book)
+    {
+        $book->update($request->only([
+            'title', 'author', 'year', 'pages', 'description'
+        ]));
+
+        return $book;
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->noContent();
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/book', [BookController::class, 'index']);
+Route::post('/book', [BookController::class, 'store']);
+Route::get('/book/{id}', [BookController::class, 'show']);
+Route::put('/book/{id}', [BookController::class, 'update']);
+Route::delete('/book/{id}', [BookController::class, 'destroy']);
